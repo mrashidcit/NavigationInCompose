b1c5c3bce6bd1d7640850693461f41c8040330c0
Rashid Saleem <mrashidcit@gmail.com>
2026-01-01 12:11:57 +0500

    lect-05: Initial code

--- app/src/main/java/com/rashid/saleem/navigationincompose/home/HomeScreen.kt
+++ app/src/main/java/com/rashid/saleem/navigationincompose/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,14 +25,14 @@ fun HomeScreen() {
     ) {
         TopAppBar(
             title = {
-                Text("Forgot Password")
+                Text("Home")
             },
             navigationIcon = {
                 IconButton(
                     onClick = { }
                 ) {
                     Icon(
-                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                        imageVector = Icons.Default.Menu,
                         contentDescription = null
                     )
                 }
